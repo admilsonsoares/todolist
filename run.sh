c98c1740b01d3d8315985f75275468ea04db8b85
admilsonsoares <admilson_soares@hotmail.com>
2022-10-09 02:13:14 -0300

    base de usuarios pre-cadastrados

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo '(-) Stopping and removing all containers'
+docker-compose down --remove-orphans
+docker stop $(docker ps -a -q)
+docker rm -f $(docker ps -a -q)
+echo '(✔) Stopped and removed all containers'
+
+echo '(-) Starting application'
+./gradlew clean build -x test
+docker-compose up --build
---
+++ sql/init-todolist.sql
@@ -0,0 +1,70 @@
+
+USE todolist;
+
+DROP TABLE IF EXISTS profile;
+
+CREATE TABLE profile (
+  id bigint(20) unsigned NOT NULL,
+  name VARCHAR(32) NOT NULL,
+  description VARCHAR(32) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO profile
+    (id, name, description)
+VALUES
+    (1, "super user", "usuario admin"),
+    (2, "default", "usuario padrao");
+
+DROP TABLE IF EXISTS userapp;
+
+CREATE TABLE userapp (
+  id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  name VARCHAR(32) NOT NULL,
+  profile_id bigint(20) unsigned DEFAULT 2,
+  PRIMARY KEY (id),
+  FOREIGN KEY (profile_id) REFERENCES profile(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO userapp
+    (name, profile_id)
+VALUES
+    ("administrator", 1);
+
+INSERT INTO userapp
+    (name)
+VALUES
+    ("amina"),
+    ("telma"),
+    ("admilson");
+
+DROP TABLE IF EXISTS status;
+
+CREATE TABLE status (
+  id bigint(20) unsigned NOT NULL,
+  name VARCHAR(32) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO status
+    (id, name)
+VALUES
+    (1, "pending"),
+    (2, "completed");
+
+DROP TABLE IF EXISTS task;
+
+CREATE TABLE task (
+  id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  name VARCHAR(32) NOT NULL,
+  description VARCHAR(32) NOT NULL,
+  created_at datetime NOT NULL,
+  updated_at datetime DEFAULT NULL,
+  user_id bigint(20) unsigned NOT NULL,
+  status_id bigint(20) unsigned DEFAULT 1,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES userapp(id),
+  FOREIGN KEY (status_id) REFERENCES status(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
